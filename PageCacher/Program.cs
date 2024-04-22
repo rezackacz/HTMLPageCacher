174883df50425d3b487bd3b47d0651b1ab0bd353
Jáchym Řezáč <jachym.rezac@student.gmh.cz>
2024-04-22 12:37:03 +0200

    ye

--- PageCacher/Program.cs
+++ PageCacher/Program.cs
@@ -2,14 +2,39 @@
 using System.Net;
 using System.Net.Http;
 namespace PageCacher {
-    public class Program {
-        public static void Main(string[] args){
-            if (args.Length == 0) Console.WriteLine("Usage: PageCacher SERVER_IP STORAGE_URL");
+  public class Program {
+    public static void Main(string[] args) {
+      if (args.Length == 0) { Console.WriteLine("Usage: PageCacher SERVER_IP STORAGE_URL"); return; }
 
-            Console.WriteLine(args[0]);
-            Console.WriteLine(args[1]);
+      Console.WriteLine(args[0]);
+      Console.WriteLine(args[1]);
+      Console.WriteLine(args[2]);
 
-            IPAddress server_ip = IPAddress.Parse(args[0]);
-        }
+      UriBuilder uriBuilderJidelna = new UriBuilder();
+      uriBuilderJidelna.Host = args[0];
+      uriBuilderJidelna.Port = 80;
+      uriBuilderJidelna.Path = "jidelna.html";
+
+      UriBuilder uriBuilderTimeTable = uriBuilderJidelna; uriBuilderTimeTable.Path = "classroomGroup" + args[2] + ".html";
+
+
+      HttpClient jidelna = new HttpClient();
+      HttpClient clsGroup = new HttpClient();
+
+
+      while (true) {
+        HttpResponseMessage jidelnaResponse = jidelna.Send(new HttpRequestMessage(HttpMethod.Get, uriBuilderJidelna.Uri));
+        HttpResponseMessage clsResponse = clsGroup.Send(new HttpRequestMessage(HttpMethod.Get, uriBuilderTimeTable.Uri));
+
+        String jidelnaHTML = jidelnaResponse.Content.ReadAsStringAsync().Result;
+        String clsHTML = clsResponse.Content.ReadAsStringAsync().Result;
+
+        File.WriteAllText(args[1] + "/jidelna.html", jidelnaHTML);
+        File.WriteAllText(args[1] + "/clsGroup.html", clsHTML);
+
+        Thread.Sleep(10000);
+      }
+
     }
+  }
 }
