827bad5a1987856e9ef78945e28f242a7fcfa791
Jáchym Řezáč <jachym.rezac@gmail.com>
2024-04-23 16:41:57 +0200

    work

--- PageCacher/Program.cs
+++ PageCacher/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Net;
 using System.Net.Http;
 namespace PageCacher {
   public class Program {
+
+    private const string no_net_html = "<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\"><title>NENÍ PŘIPOJENO</title></head><body style=\"display: flex;height: 100%;font-size: xxx-large;flex-flow: column;justify-content: center;font-family: sans-serif;text-align: center;\"><h1 id=\"main\">Připojení k serveru se nepodařilo.</h1><h2 style=\"font-family: monospace;text-align: center;\">Síť nemusí být k dispozici, nebo nastal problém se serverem.</h2><style type=\"text/css\"></style><deepl-input-controller></deepl-input-controller></body></html>";
+
     public static void Main(string[] args) {
       if (args.Length == 0) { Console.WriteLine("Usage: PageCacher SERVER_IP CACHE_PATH CLASSROOM_GROUP_ID"); return; }
 
@@ -34,7 +38,7 @@ namespace PageCacher {
           clsResponse = clsGroup.Send(new HttpRequestMessage(HttpMethod.Get, uriBuilderTimeTable.Uri));
         } catch (Exception e) {
           Console.WriteLine("Download failed :(.");
-
+          NetworklessRender(cache_path);
           goto SLEEP_AND_REPEAT;
         }
 
@@ -49,11 +53,16 @@ namespace PageCacher {
         Thread.Sleep(10000);
       }
     }
-    private void NoNetwork(string cache_path) {
+    private static void NetworklessRender(string cache_path) {
       if (File.Exists(cache_path + "/jidelna.html") && File.Exists(cache_path + "/clsGroup.html")) {
+        string jidelnaHTML = File.ReadAllText(cache_path + "/jidelna.html");
+        string clsGroupHTML = File.ReadAllText(cache_path + "/clsGroup.html");
+
+        
 
       } else {
-
+        File.WriteAllText(cache_path + "/jidelna.html", no_net_html);
+        File.WriteAllText(cache_path + "/clsGroup.html", no_net_html);
       }
 
 
