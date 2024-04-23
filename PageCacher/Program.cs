9a4c9e369a02081e91eac8405e392aae3240f438
Jáchym Řezáč <jachym.rezac@student.gmh.cz>
2024-04-23 12:15:16 +0200

    start adding new features making offline suck less

--- PageCacher/Program.cs
+++ PageCacher/Program.cs
@@ -6,18 +6,18 @@ namespace PageCacher {
     public static void Main(string[] args) {
       if (args.Length == 0) { Console.WriteLine("Usage: PageCacher SERVER_IP CACHE_PATH CLASSROOM_GROUP_ID"); return; }
 
-      Console.WriteLine(args[0]);
-      Console.WriteLine(args[1]);
-      Console.WriteLine(args[2]);
+      Console.WriteLine(args[0]); string IP = args[0];
+      Console.WriteLine(args[1]); string cache_path = args[1];
+      Console.WriteLine(args[2]); string clsgroupid = args[2];
 
       UriBuilder uriBuilderJidelna = new UriBuilder();
-      uriBuilderJidelna.Host = args[0];
+      uriBuilderJidelna.Host = IP;
       uriBuilderJidelna.Port = 80;
       uriBuilderJidelna.Path = "jidelna.html";
 
       UriBuilder uriBuilderTimeTable = new();
-      uriBuilderTimeTable.Path = "classroomGroup" + args[2] + ".html";
-      uriBuilderTimeTable.Host = args[0];
+      uriBuilderTimeTable.Path = "classroomGroup" + clsgroupid + ".html";
+      uriBuilderTimeTable.Host = IP;
       uriBuilderTimeTable.Port = 80;
 
 
@@ -34,20 +34,32 @@ namespace PageCacher {
           clsResponse = clsGroup.Send(new HttpRequestMessage(HttpMethod.Get, uriBuilderTimeTable.Uri));
         } catch (Exception e) {
           Console.WriteLine("Download failed :(.");
+
           goto SLEEP_AND_REPEAT;
         }
 
         String jidelnaHTML = jidelnaResponse.Content.ReadAsStringAsync().Result;
         String clsHTML = clsResponse.Content.ReadAsStringAsync().Result;
 
-        File.WriteAllText(args[1] + "/jidelna.html", jidelnaHTML);
-        File.WriteAllText(args[1] + "/clsGroup.html", clsHTML);
-        
+        File.WriteAllText(cache_path + "/jidelna.html", jidelnaHTML);
+        File.WriteAllText(cache_path + "/clsGroup.html", clsHTML);
 
-        SLEEP_AND_REPEAT:
+
+      SLEEP_AND_REPEAT:
         Thread.Sleep(10000);
       }
+    }
+    private void NoNetwork(string cache_path) {
+      if (File.Exists(cache_path + "/jidelna.html") && File.Exists(cache_path + "/clsGroup.html")) {
+
+      } else {
+
+      }
+
+
 
     }
+
   }
+}
 }
